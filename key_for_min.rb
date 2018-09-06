5d4b7c10c5da74086e9d8df401fce8c488c4ff11
Ignacio R Espino <irespino@capozios.com>
2018-09-06 20:34:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_value = 0
   smallest_key = ''
   name_hash.each do |key, value|
-    if value > smallest_value
+    if value < smallest_value
       smallest_value = value
       smallest_key = key
     end
